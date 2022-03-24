11ce4b85458a063e1cfc5a5513e41a30767771e7
anton-lapin <lapin_antoine@mail.ru>
2022-03-24 20:54:37 +0300

    2. Сокобан. Тестовое задание. [#75283 #279462]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -7,6 +7,7 @@ public class Win {
             if (board[index][index] == 1
                     && (fullHorizontal(board, index) || fullVertical(board, index))) {
                 rsl = true;
+                break;
             }
         }
         return rsl;
